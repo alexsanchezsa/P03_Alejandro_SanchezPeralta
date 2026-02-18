9925686369a661e1c36442d35413d92f999ff403
alexsanchezsa <sanchez.peale23@zaragoza.salesianos.edu>
2026-02-18 18:43:26 +0100

    Fix migracion Spring Boot 4.0: corregir imports Jackson y autoconfigure

--- src/test/java/com/aura/reviews/ClienteE2ETest.java
+++ src/test/java/com/aura/reviews/ClienteE2ETest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.resttestclient.TestRestTemplate;
-import org.springframework.boot.resttestclient.AutoConfigureTestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
